1eb089000a11dd3aaef84fdfe09e62c3452f6ac7
sidawasthi420 <144908193+sidawasthi420@users.noreply.github.com>
2025-03-30 15:48:54 +0530

    Update Run.java

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/",
         glue = {"StepDefinitions"},
         tags = "@regression",
-        publish = false
+        publish = false,
         plugin = {"pretty", "json:target/cucumber-report.json" }
 
 )
